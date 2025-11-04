d50371e7f9d77e99e1e7e571f9d81784c97f4c3c
BornaVuksanovic <boco.vuksanovic2@gmail.com>
2025-11-04 09:07:26 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // da mozemo pristupiti usernameu, emailu i passwordu u authorizaciji
+app.use(cors());    // dozvoljava zahtjeve iz drugih domena,portova npr. 3000 i 5000
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    connectDB();
+});
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,111 @@
+import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import Book from "../models/Book.js";
+import protectRoute from "../middleware/auth.middleware.js";
+import e from "express";
+
+
+const router = express.Router();
+
+router.post("/", protectRoute, async (req, res) => {
+    try {
+        const { title, caption, rating, image } = req.body;
+
+        if(!title || !image || !caption || !rating){
+            return res.status(400).json({ message: "Please provide all fiels" });
+        }
+
+        // uploadaj sliku u cloudinary
+        const uploadResponse = await cloudinary.uploader.upload(image);
+        const imageUrl = uploadResponse.secure_url;
+
+        const newBook = new Book({ 
+            title,
+            caption,
+            rating,
+            image: imageUrl,
+            user: req.user._id,
+        })
+
+    } catch (error) {
+        console.log("Error creating book", error);
+        res.status(500).json({ message: error.message });
+    }
+})
+
+
+
+// zelimo dohvacat 5 po 5 knjiga a ne sve odjednom
+router.get("/", protectRoute, async (req, res) => {
+    // const response = await fetch("http://localhost:3000/api/books?page=3&limit=5");
+    try{
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 5;
+        const skip = (page - 1) * limit;
+
+        const books = await Book.find()
+        .sort({ createdAt: -1 })    // prvo najnovije
+        .skip(skip) 
+        .limit(limit)
+        .populate("user", "username profileImage");
+        
+        const totalBooks = await Book.countDocuments();
+
+        res.send({
+            books,
+            currentPage: page,
+            totalBooks,
+            totalPages: Math.ceil(totalBooks / limit),
+        })
+
+    } catch (error) {
+        console.log("Error in get all books route", error);
+        res.status(500).json({ message: "internal server error" });
+    }
+})
+
+// dohvati knjige od logiranog usera
+router.get("/user", protectRoute, async (req, res) => {
+    try {
+        const books = await Book.find({ user: req.user._id }).sort({ createdAt: -1 });
+        res.json(books);
+    } catch (error) {
+        console.error("Get user books error:", error.message);
+        res.status(500).json({ message: "Server error" });  
+    }
+})
+
+router.delete("/:id", protectRoute, async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        if (!book) return res.status(404).json({ message: "Book not found" });
+
+        // provjeri je li user kreator knjige
+        if (book.user.toString() !== req.user._id.toString()){
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        // obrisis sliku s clouinarya
+        // https://res.cloudinary.com/de1rm4uto/image/upload/v124322342/qyup61vejflxx2igvi0.png
+
+        if(book.image || book.image.includes("cloudinary")) {
+            try {
+                const publicId = book.image.split("/").pop().split(".")[0];
+                await cloudinary.uploader.destroy(publicId);
+            } catch (error) {
+                console.log("Error deleting image from cloudinary", error);
+            }
+        }
+
+        await book.deleteOne();
+
+        res.json({ message: "Book deleted successfully" });
+
+
+    } catch (error) {
+        console.log("Error deleting book", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+export default router;
